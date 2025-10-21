d6404d72063465ae4fc94b3baa87e4f74f438674
Homebrew Publisher <noreply@disney.com>
2025-10-21 03:28:11 -0700

    Update print-param to 1.0.18

--- Formula/print-param.rb
+++ Formula/print-param.rb
@@ -1,16 +1,16 @@
 class PrintParam < Formula
   desc "Command-line tool to print parameters"
   homepage "https://github.com/Hamdih4/print-param"
-  version "1.0.17"
+  version "1.0.18"
 
   on_arm do
-    url "https://github.com/Hamdih4/print-param/releases/download/v1.0.17/print-param_Darwin_arm64.tar.gz"
-    sha256 "8d22ed527c7143dbe41c298afb95c048ec1ac84ca0b91ffa2a99dc8a2afc4683"
+    url "https://github.com/Hamdih4/print-param/releases/download/v1.0.18/print-param_Darwin_arm64.tar.gz"
+    sha256 "fd5146c6d7b06374b621c81ab0da1b74418bb19435b191dcafdca8eb1ef48364"
   end
 
   on_intel do
-    url "https://github.com/Hamdih4/print-param/releases/download/v1.0.17/print-param_Darwin_x86_64.tar.gz"
-    sha256 "6133bb49b2a664214a931bc7714e96850c2e5c94c572dfa54d48f30b167f9d41"
+    url "https://github.com/Hamdih4/print-param/releases/download/v1.0.18/print-param_Darwin_x86_64.tar.gz"
+    sha256 "ea1bd3b29cb90ed611b92d09fab04a83fa5c067fad227128ef589921ed1d7f48"
   end
 
   def install
