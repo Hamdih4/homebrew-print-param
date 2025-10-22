75698a6420abc1d4f228a0a571bbd231f08ab575
Homebrew Publisher <noreply@disney.com>
2025-10-21 17:12:55 -0700

    Update print-param to 1.0.22

--- Formula/print-param.rb
+++ Formula/print-param.rb
@@ -5,12 +5,12 @@ class PrintParam < Formula
 
   on_arm do
     url "https://github.disney.com/alhah004/print-param/releases/download/v1.0.22/print-param_Darwin_arm64.tar.gz"
-    sha256 "c5600b7dab677cfadb37a7ec07a6d3a6792c8b3cfc1620cec7b1b58819c4bcfb"
+    sha256 "9a257d83b9d7e54e9357894916b11d2a749c077e7a9ac36912e28c03bdd8640c"
   end
 
   on_intel do
     url "https://github.disney.com/alhah004/print-param/releases/download/v1.0.22/print-param_Darwin_x86_64.tar.gz"
-    sha256 "1fc95e2bdcc4e58d5e6161c0f376ce94989046c5540bc48d7b2d8d317e16b1a2"
+    sha256 "911ce45b7ae1188ab8b7d91ea65df3074c3685af78f877f1e85515321e7b4b8f"
   end
 
   def install
